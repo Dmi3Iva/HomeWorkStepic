8c864b38737af62022bdf04f5529f439dfd8567d
DmitriyIvanov <Diklacway@gmail.com>
2018-11-05 21:58:46 +0200

    second edit

--- 1.9/hello.py
+++ 1.9/hello.py
@@ -1,9 +1,17 @@
-def wsgi_app(environ, start_responce){
-    #/?a=1&a=2&b=3
-    status = '200 OK'
-    headers =  [
-        ('Content-Type', ' text/plain')
-    ]
-    start_responce(status, headers)
-    return ["\n".join(environ.get('QUERY_STRING').split("&"))]
-}
+from cgi import parse_qs
+
+def application(environ, start_response):
+
+  query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+  body = []
+  for key, values in query.items():
+    for item in values:
+      body.append(key + "=" + item + "\r\n")
+
+  status = '200 OK'
+  headers = [
+   ('Content-Type', 'text/plain')
+  ]
+
+  start_response(status, headers)
+  return body
